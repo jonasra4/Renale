c498e8d7c6f4dd22faf0bc0ccb37a611bb172084
Oziel Alves <ozielalves@ufrn.edu.br>
2019-01-28 12:40:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
+// MODAL JS
+
+// Get the modal
+var modal_0 = document.getElementById('Modal_0');
+var modal_1 = document.getElementById('Modal_1');
+var modal_2 = document.getElementById('Modal_2');
+
+// Get the button that opens the modal
+var btn_0 = document.getElementById("Btn_0");
+var btn_1 = document.getElementById("Btn_1");
+var btn_2 = document.getElementById("Btn_2");
+
+// Get the <span> element that closes the modal
+var span_0 = document.getElementsByClassName("close_0")[0];
+var span_1 = document.getElementsByClassName("close_1")[0];
+var span_2 = document.getElementsByClassName("close_2")[0];
+
+// When the user clicks on the button, open the modal 
+btn_0.onclick = function() {
+  modal_0.style.display = "block";
+}
+
+btn_1.onclick = function() {
+  modal_1.style.display = "block";
+}
+
+btn_2.onclick = function() {
+  modal_2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span_0.onclick = function() {
+  Modal_0.style.display = "none";
+}
+
+span_1.onclick = function() {
+  Modal_1.style.display = "none";
+}
+
+span_2.onclick = function() {
+  Modal_2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal_0 || event.target == modal_1 || event.target == modal_2) {
+    modal_0.style.display = "none";
+    modal_1.style.display = "none";
+    modal_2.style.display = "none";
+  }
+}
+
 //EFEITO NAVBAR
 jQuery(document).ready(function ($) {
 
